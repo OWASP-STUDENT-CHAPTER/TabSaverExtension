d7e461ac0a49e1572281376363a8b14671b6a472
Aman Kumar <aman.kumar11112001@gmail.com>
2021-10-10 22:58:51 +0530

    Tabs are displayed now

---
+++ content.js
@@ -0,0 +1,7 @@
+function saveText(text, filename){
+    var a = document.createElement('a');
+    a.setAttribute('href', 'data:text/plain;charset=utf-8,'+encodeURIComponent(text));
+    a.setAttribute('download', filename);
+    a.click();}
+  
+  saveText( JSON.stringify(tabsInfo), "tabs.json" );
