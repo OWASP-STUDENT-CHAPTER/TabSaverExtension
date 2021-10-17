70c3eee0aeee6cad918708f3453461fb1344b545
Aman Kumar <aman.kumar11112001@gmail.com>
2021-10-17 12:50:30 +0530

    workspace array visible

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 const btnsave = document.getElementById('savebtn');
 const btnset = document.getElementById('getbtn');
+const btnAdd = document.getElementById('addBtn');
+const btnAll = document.getElementById('workspacebtn');
+const workspacediv = document.getElementById('workspaces');
 
 
 // var gettingAll;
@@ -37,5 +40,29 @@ function callback1(){
 
 //chrome.tabs.executeScript(null,{file: "content.js"}); 
 
+//add button to display second section
+btnAdd.addEventListener('click', () => {
+  console.log('wokring');
+  document.getElementById('all_workspace').style.display = 'none';
+  document.getElementById('create_workspace').style.display = 'block';
+});
 
+//all button to display first section
+btnAll.addEventListener('click', () => {
+  document.getElementById('all_workspace').style.display = 'block';
+  document.getElementById('create_workspace').style.display = 'none';
+});
 
+//to get all the workspaces (currently static)
+var arr =['w1','w2','w3','w4','w5'];
+function printBtn() {
+  for(var i=0; i<arr.length; i++)
+  {
+      var btn = document.createElement("button"); 
+      btn.className = "button"
+      var t = document.createTextNode(arr[i]);
+      btn.appendChild(t);
+      workspacediv.appendChild(btn);
+  }
+} 
+printBtn();
